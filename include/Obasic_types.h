9255ccc99e9432af21dc4fc16888c57af5e34ebb
jiqiubo <jiqb@onbonbx.com>
2020-02-14 17:22:38 +0800

    调整目录结构及名称；修改说明文档；

---
+++ include/Obasic_types.h
@@ -0,0 +1,50 @@
+﻿/**************************************************************
+ * All rights reserved. No part of this file may be reproduced
+ * in any form by print, photo print, microfilm or any other
+ * means without written permission by ONBON.\n
+ * (C) 2010-2019 Onbon
+ *************************************************************/
+
+/*!@file 	Obasic_types.h
+ * @brief 	onbon basic types definition
+ * @author 	fantx
+ * @version	1.0.0
+ * @date	2018-12-13
+ * @note	none
+ * @bug 	none
+ * @todo 	none
+ */
+
+
+#ifndef __obasic_types_h__
+#define __obasic_types_h__
+
+/*!@name Legacy types
+ * Deprecated legacy types.
+ * @{
+ */
+
+//#include <stdio.h>
+
+typedef unsigned char			Ouint8;		//!< unsigned 8-bit
+typedef char			        Oint8;		//!< signed 8-bit 
+typedef unsigned short			Ouint16;	//!< unsigned 16-bit 
+typedef short			        Oint16;		//!< signed 16-bit 
+typedef unsigned int			Ouint32;	//!< unsigned 32-bit
+typedef int			            Oint32;		//!< signed 32-bit
+typedef unsigned long long      Ouint64;        //!< unsigned 64-bit
+typedef long long               Oint64;         //!< singed 64-bit
+typedef float		            Ofloat32;	//!< 32-bit floating point
+typedef double                  Ofloat64;	//!< 64-bit double precision FP
+typedef bool                    Obool;
+#ifndef TRUE
+#  define TRUE		1				//!< true
+#endif
+
+#ifndef FALSE
+#  define FALSE		0				//!< false
+#endif
+
+/*@}*/
+
+#endif //__obasic_types_h__
---
+++ include/README.md
@@ -0,0 +1,7 @@
+# 文件说明
+
+bx_dual_sdk.h和Obasic_types.h是仰邦SDK动态库头文件；
+bx_sdk_dual.h中包含了早期未使用bxDual_开头的函数的宏定义，方便老用户仍可以使用原来的函数名称；
+
+
+
